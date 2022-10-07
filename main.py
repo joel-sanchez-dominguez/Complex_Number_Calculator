a658ce4a59502781139e55b58b0c01d1a3894351
joel-sanchez-dominguez <joel.iprj@gmail.com>
2022-10-07 16:32:58 -0300

    Updated 06102022

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 """
-This is the python script to do algebric operations with cmplex numbers
+A basic complex number calculator to do simples algebrics operations (+, -, *, /),
+complex numbers in retangular ou polar form.
+(+) and (-) operations required numbers in retangular form
+(*) and (/) operations required numbers in polar form
+
+This is the python script to do algebric operations with complex numbers
 
 A GUI is created to interact with users:
 
@@ -7,6 +12,8 @@ A GUI is created to interact with users:
 2-) Select the algebric operation (+, -, *, /)
 3-) Click on Compute
 4-) Results and history are updated
+
+Important: In linux systems coment line --> # window.iconbitmap("circuit.ico")
 
 Enjoyed
 
